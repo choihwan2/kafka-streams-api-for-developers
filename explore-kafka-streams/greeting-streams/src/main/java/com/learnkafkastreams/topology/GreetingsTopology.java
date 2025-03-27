27b5186d4e0b66b88b8e16ce78d0521968c4c8e7
pavlo.v <pavlo.v@kakaocorp.com>
2025-03-27 16:34:54 +0900

    Section5-2 flatmap

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -1,11 +1,15 @@
 package com.learnkafkastreams.topology;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class GreetingsTopology {
     public static String GREETINGS = "greetings";
@@ -17,8 +21,16 @@ public class GreetingsTopology {
 
         var greetingStream = streamsBuilder.stream(GREETINGS, Consumed.with(Serdes.String(), Serdes.String()));
         greetingStream.print(Printed.<String, String>toSysOut().withLabel("greetingStream"));
-        var modifiedStream = greetingStream.filter((key, value) -> value.length() > 5).
-                mapValues((readonlyKey, value) -> value.toUpperCase());
+        var modifiedStream = greetingStream
+//                .filter((key, value) -> value.length() > 5).
+//                mapValues((readonlyKey, value) -> value.toUpperCase());
+//                .map((key, value) -> KeyValue.pair(key.toUpperCase(), value.toUpperCase()));
+                .flatMap((key, value) -> {
+                    var newValues = Arrays.asList(value.split(""));
+                    return newValues.stream()
+                            .map(val -> KeyValue.pair(key, val))
+                            .collect(Collectors.toList());
+                });
         modifiedStream.print(Printed.<String, String>toSysOut().withLabel("modifiedStream"));
         modifiedStream.to(GREETINGS_UPPERCASE, Produced.with(Serdes.String(), Serdes.String()));
 
