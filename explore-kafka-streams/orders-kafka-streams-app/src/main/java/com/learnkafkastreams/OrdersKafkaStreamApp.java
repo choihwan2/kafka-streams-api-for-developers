a082814e38617d4de735f5670e5f516bf32b2666
choihwan2 <choihwan2@naver.com>
2025-04-01 20:44:47 +0900

    section 26 오타 수정

--- explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/OrdersKafkaStreamApp.java
+++ explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/OrdersKafkaStreamApp.java
@@ -21,22 +21,22 @@ public class OrdersKafkaStreamApp {
     public static void main(String[] args) {
 
         // create an instance of the topology
-        Topology topology=  OrdersTopology.buildToplogy();
+        Topology topology = OrdersTopology.buildToplogy();
 
         Properties config = new Properties();
         config.put(StreamsConfig.APPLICATION_ID_CONFIG, "orders-app"); // consumer group
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest"); // read only the new messages
-        createTopics(config, List.of(OrdersTopology.ORDERS));
+        createTopics(config, List.of(OrdersTopology.RESTAURANT_ORDERS, OrdersTopology.GENERAL_ORDERS));
 
         //Create an instance of KafkaStreams
         var kafkaStreams = new KafkaStreams(topology, config);
 
         //This closes the streams anytime the JVM shuts down normally or abruptly.
         Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
-        try{
+        try {
             kafkaStreams.start();
-        }catch (Exception e ){
+        } catch (Exception e) {
             log.error("Exception in starting the Streams : {}", e.getMessage(), e);
         }
 
@@ -46,11 +46,11 @@ public class OrdersKafkaStreamApp {
 
         AdminClient admin = AdminClient.create(config);
         var partitions = 1;
-        short replication  = 1;
+        short replication = 1;
 
         var newTopics = greetings
                 .stream()
-                .map(topic ->{
+                .map(topic -> {
                     return new NewTopic(topic, partitions, replication);
                 })
                 .collect(Collectors.toList());
@@ -61,7 +61,7 @@ public class OrdersKafkaStreamApp {
                     .all().get();
             log.info("topics are created successfully");
         } catch (Exception e) {
-            log.error("Exception creating topics : {} ",e.getMessage(), e);
+            log.error("Exception creating topics : {} ", e.getMessage(), e);
         }
     }
 
